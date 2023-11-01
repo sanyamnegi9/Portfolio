0d44f5fc200fb93ccac0393908edcc3261ece6ca
Sanyam <Sanyamnegi@proton.me>
2023-11-01 11:39:25 +0530

    restart

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import { Route, Routes } from 'react-router-dom';
+import logo from './logo.svg';
 import './App.css';
-import Navbar from './Components/Navbar';
-import Home from './Pages/Home';
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-      <Home />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
